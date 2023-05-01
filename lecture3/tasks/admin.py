817d6d9bd71adef74e5c30fa5107339786cc35f5
shindok <shindok8@gmail.com>
2023-05-01 13:47:46 -0400

    included admin function, build models for db representations, and began updating html to interact directly with db

--- lecture3/tasks/admin.py
+++ lecture3/tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Status, Task
+
 # Register your models here.
+
+admin.site.register(Status)
+admin.site.register(Task)
--- lecture3/tasks/urls.py
+++ lecture3/tasks/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+
 from . import views
 
 
+
+
+
 app_name = "tasks"
+
 urlpatterns = [
+
     path('', views.index, name='index'),
-    path('add', views.add, name='add'),
+
+    path('add', views.add_task, name='add'),
+
 ]
